0e430db5dab47b14d4b6f96be3467f55fd1b0107
mateusgaudencio14 <mateusdvlp@gmail.com>
2020-01-24 22:40:32 -0200

    criando a estrutura do src e apontando o app.js

---
+++ src/pages/estados/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+export default class Estados extends React.Component{
+	
+	render(){
+		return(
+			<View style={styles.container}>
+				<Text>Estados View</Text>
+			</View>
+		);
+	}
+
+}
